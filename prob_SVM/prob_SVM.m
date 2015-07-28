f76cdc2dfd3baddb1fbceaf3197f780b2200d9ea
98Chamberlain <a071442002@gmail.com>
2015-07-29 01:58:46 +0800

    finish code for training

--- prob_SVM/prob_SVM.m
+++ prob_SVM/prob_SVM.m
@@ -12,23 +12,34 @@ load('./gt_scene.mat');
 % groundtruth 1*40 cell
 % gt_scene 1*205 double
 
-% gt = 205 * 40
-gt = zeros(nScene,nLabel);
-for i = 1:nScene
-    if i == 94
-        gt(i,[1,2,6,7]) = 1;
-    elseif i == 123
-        gt(i,[1,3,4,38,39]) = 1;
-    elseif (sum([48,66,91,103,162,164] == i) >= 1)
-        gt(i,[1,3,4,12,31,35]) = 1;
-    elseif (sum([2,11,73] == i) >= 1)
-        gt(i,[1,2,9]) = 1;
-    elseif (sum([30,31] == i) >= 1)
-        gt(i,[1,2,5]) = 1;
-    elseif gt_scene(i)~=0
-        gt(i,groundtruth{gt_scene(i)}) = 1;
-    end
-end
+% % gt = 205 * 40
+% gt = zeros(nScene,nLabel);
+% for i = 1:nScene
+%     if i == 94
+%         gt(i,[1,2,6,7]) = 1;
+%     elseif i == 123
+%         gt(i,[1,3,4,38,39]) = 1;
+%     elseif (sum([48,66,91,103,162,164] == i) >= 1)
+%         gt(i,[1,3,4,12,31,35]) = 1;
+%     elseif (sum([2,11,73] == i) >= 1)
+%         gt(i,[1,2,9]) = 1;
+%     elseif (sum([30,31] == i) >= 1)
+%         gt(i,[1,2,5]) = 1;
+%     elseif gt_scene(i)~=0
+%         gt(i,groundtruth{gt_scene(i)}) = 1;
+%     end
+% end
+
+groundtruth{41} = [1,2,6,7];
+gt_scene(94) = 41;
+groundtruth{42} = [1,3,4,38,39];
+gt_scene(123) = 42;
+groundtruth{43} = [1,3,4,12,31,35];
+gt_scene([48,66,91,103,162,164]) = 43;
+groundtruth{44} = [1,2,9];
+gt_scene([2,11,73]) = 44;
+groundtruth{45} = [1,2,5];
+gt_scene([30,31]) = 45;
 
 root_s = [1,2,3,5:1:22,24,25,26,28:1:38,40:1:49,51:1:63,65,66,...
     67,69:1:75,77:1:81,83:1:94,96:1:99,101:1:119,121:1:138,140,142,143,145,...
@@ -42,16 +53,22 @@ root_s = [1,2,3,5:1:22,24,25,26,28:1:38,40:1:49,51:1:63,65,66,...
 % ----- Training -----
 addpath('../Research_Toolkit/SVM/libsvm-3.20/matlab');
 
-model{1,40} = [];
-mf{1,40} = [];
-nrm{1,40} = [];
-tmp = repmat(root_s,[train_amt,1]);
-lb_tmp = tmp(:);
+% model{1,40} = [];
+% mf{1,40} = [];
+% nrm{1,40} = [];
+% tmp = repmat(root_s,[train_amt,1]);
+% lb_tmp = tmp(:);
+% 
+% features = training_SR;
+% for i = 1:nLabel
+%     label = gt( lb_tmp , i);
+%     [model{i},mf{i},nrm{i}] = training_svm( features , label );
+% end
 
+label = gt_scene(root_s);
+label = repmat(label,[train_amt,1]);
+label = label(:);
 features = training_SR;
-for i = 1:nLabel
-    label = gt( lb_tmp , i);
-    [model{i},mf{i},nrm{i}] = training_svm( features , label );
-end
+[model,mf,nrm] = training_svm( features , label );
 
 end
